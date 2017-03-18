6ec20b43f442b25a3b374e58b02b6e8a243adf1a
asimolmez <asimolmez@outlook.com>
2017-03-19 01:01:52 +0300

    example middleware added.

---
+++ app/redux/middlewares/Logging.js
@@ -0,0 +1,9 @@
+function patchStoreToAddLogging(store) {
+  let next = store.dispatch
+  store.dispatch = function dispatchAndLog(action) {
+    console.log('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    return result
+  }
+}
